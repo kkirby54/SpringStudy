b38a4a14f700b25021bf51d175499fcbf5d78620
kkirby54 <sdk926@gmail.com>
2022-02-16 01:24:24 +0900

    HttpServlet - Response

---
+++ MVC-lecture/servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,33 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("KIM");
+        helloData.setAge(20);
+
+        // {"username":"KIM", "age":20}로 만들어서 보내야 한다.
+
+        String result = objectMapper.writeValueAsString(helloData);
+        response.getWriter().write(result);
+
+    }
+}
