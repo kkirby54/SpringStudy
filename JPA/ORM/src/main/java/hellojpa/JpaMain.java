4a00d174fbbfbe9f218e086481520ef3a7d3d825
kkirby54 <sdk926@gmail.com>
2022-05-05 15:40:33 +0900

    JPQL

--- JPA/ORM/src/main/java/hellojpa/JpaMain.java
+++ JPA/ORM/src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,10 @@
 package hellojpa;
 
+import hellojpa.domain.Book;
+import hellojpa.domain.Item;
+import hellojpa.domain.Member;
+import hellojpa.domain.Order;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -17,15 +22,11 @@ public class JpaMain {
 
         try {
 
-            // 수정 -> JPA가 관리하기 때문에. 바뀌면 이걸 감지한다. 변경 감지.
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("HelloJPA");
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("asd");
 
-            List<Member> findMembers = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
-            for (Member findMember : findMembers) {
-                System.out.println("findMember = " + findMember.getName());
-            }
+            em.persist(book);
 
             tx.commit();
         } catch (Exception e){
---
+++ JPA/ORM/src/main/java/hellojpa/domain/Delivery.java
@@ -0,0 +1,20 @@
+package hellojpa.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "DELIVERY_ID")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
+
+    private String city;
+    private String street;
+    private String zipcode;
+    private DeliveryStatus deliveryStatus;
+
+}
